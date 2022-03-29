553438a64942702dc854072a99b83c324d1ba79d
Lawrence Marquez <LMarquez@kerncounty.com>
2022-03-29 16:31:45 -0700

    added error checking no jobs and made pretty output

--- scraper.py
+++ scraper.py
@@ -3,10 +3,21 @@ from plyer import notification
 import requests
 from bs4 import BeautifulSoup
 
+no_job = "No jobs match your search criteria"
 # Query must have the sorting by date and requires a isDecendingSort boolean.
 location = input("Enter your city: ")
+print("\n")
+
 state = input("Enter your state (Two letter initials ONLY): ") 
+print("\n")
+
 keyword = input("Enter the type of job you are looking for (No input for all job search): ")
+print("\n")
+
+print("     +=======================================+")
+print("     +            Available Jobs             +")
+print("     +=======================================+")
+print("\n")
 
 if (keyword.strip() == ""):
     keyword = ""
@@ -19,7 +30,6 @@ url = "https://www.governmentjobs.com/jobs?{}&location={}%2C+{}&sort=date&isDesc
 r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
 
-
 #In this case, we want to look for all list classes with the name job-item
 # they will also contain a data job id which i might be able to use for something. not sure.
 # maybe use that id to make a link to whatever job might be posted.
@@ -29,8 +39,19 @@ soup = BeautifulSoup(r.text, "html.parser")
 # to be able to go through the entire site in terms of finishing all the pages, I would have
 # to figure out how to change the link after the end of a page.
 
-for job_listing in soup.find_all("li", {"class":"job-item"}):
-    for job_link in soup.find_all("a", {"class":"job-details-link"}):
-        print(job_link.prettify())
+# error checking for no jobs found:
+# need to access the h2 tag that says "No jobs match your search criteria"
+# if i get that message, i want to print a message saying no jobs in your search available.
 
-print(url)
+
+
+if (soup.find('div', {"id":"job-list-container"}).h2.contents == no_job):
+    for job_listing in soup.find_all("li", {"class":"job-item"}):
+        for job_link in soup.find_all("a", {"class":"job-details-link"}):
+            print(job_link.string)
+            print("\n")
+else:
+    print("     +=======================================+")
+    print("     +  No jobs match your search criteria!  +")
+    print("     +=======================================+")
+    print("\n")
